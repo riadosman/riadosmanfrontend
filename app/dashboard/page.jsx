22b59add48721da3e6b80d06f46b64f59687396e
Riad osman <riyados973@gmail.com>
2025-08-08 16:28:20 +0300

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import isAuth from "../components/isAuth";
 import { useRouter } from "next/navigation";
 
