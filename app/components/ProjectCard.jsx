71fb4e26ad2ae8bd1601bf97295bfb6e4fa1ea2b
riad osman <riyados973@gmail.com>
2025-08-12 17:26:35 +0300

    edit project card to make it fixed and more suitable

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -1,11 +1,17 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
 function ProjectCard({ project }) {
+  const [showFullText, setShowFullText] = useState(false);
+  function truncateText(text, maxLength) {
+    if (!text) return "";
+    return text.length > maxLength ? text.slice(0, maxLength) + "..." : text;
+  }
+
   return (
-    <div className="p-6 bg-white/5 border border-white/10 rounded-2xl shadow-lg overflow-hidden backdrop-blur-sm hover:shadow-xl hover:bg-white/10 transition-all duration-300 transform hover:-translate-y-1">
+    <div className="min-h-[380px] max-w-[373px] p-6 bg-white/5 border border-white/10 rounded-2xl shadow-lg overflow-hidden backdrop-blur-sm hover:shadow-xl hover:bg-white/10 transition-all duration-300 transform hover:-translate-y-1">
       <Image
         src={project.image}
         alt="Service 1"
@@ -14,7 +20,21 @@ function ProjectCard({ project }) {
         height={500}
       />
       <p className="text-2xl font-bold mt-4">{project.title}</p>
-      <p className="text-sm my-3">{project.description}</p>
+      <p className="text-sm my-3">
+        {showFullText
+          ? project.description
+          : truncateText(project.description, 100)}
+        {project.description.length > 100 && (
+          <span
+            className="text-main-secondary-color"
+            onClick={() => {
+              setShowFullText(!showFullText);
+            }}
+          >
+            {showFullText ? "Show Less" : "Show More"}
+          </span>
+        )}
+      </p>
       {project.livelink && (
         <Link
           href={project.livelink || "/"}
